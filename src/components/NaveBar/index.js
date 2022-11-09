e012d18bdb68511c74c99bb6f15a2e96aa8c454b
Ahmad-Qerem <alqerema@gmail.com>
2022-11-09 18:54:32 +0200

    first commit here

---
+++ src/components/NaveBar/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NaveBar = () => {
+    const Text = ["Home","About Us","Contact Us"];
+  return (
+    <div>
+        <div>
+            <div>Hello</div>
+            <div>Hello2</div>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default NaveBar
